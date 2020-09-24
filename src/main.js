33fb62b317dfa7b639dc58dc356d0bb4f7ed1b44
chenc <chenc@fengyuntec.com>
2020-09-25 02:01:48 +0800

    fix: 导入各个模块

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./plugins/element.js";
-
+import "@/assets/scss/common.scss";
+import "./plugins";
+import "./components";
+import "./common/directive";
+import "./common/filter";
+import "./common/prototype";
+import "./services";
 Vue.config.productionTip = false;
 
 new Vue({
