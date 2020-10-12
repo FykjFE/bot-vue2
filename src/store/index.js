96cd7e22d73aa03d049b89148f7fd110d63c23ed
zzfn <44751363+zzfn@users.noreply.github.com>
2020-10-12 12:48:52 +0800

    feat: 添加统一的vuex方法 (#12)
    * docs: 添加git提交规则
    
    * chore: 修改代码提交规范，移除生成工具
    
    * feat: 添加composition-api
    
    * docs: 添加待完善新功能
    
    * docs: 修改使用文档
    
    * feat: 添加统一的vuex方法

--- src/store/index.js
+++ src/store/index.js
@@ -10,11 +10,73 @@ const files = require.context("./modules", true, /\.js$/);
 const modules = files.keys().reduce((prev, curr) => {
   return { ...prev, [/[^./].+(?=\.js$)/.exec(curr)[0]]: files(curr).default };
 }, {});
-export default new Vuex.Store({
-  // todo根目录设置store
+
+const store = new Vuex.Store({
   state: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    setKeyValue(state, { module, key, value, merge = true }) {
+      let _state = module ? state[module] : state;
+      if (!_state) {
+        console.error(`没有找到${module}模块`);
+        return;
+      }
+      let flag =
+        Object.prototype.toString.call(_state[key]) === "[object Object]";
+      let flag2 = Object.prototype.toString.call(value) === "[object Object]";
+      if (flag && flag2 && merge) {
+        _state[key] = { ..._state[key], ...value };
+      } else {
+        _state[key] = value;
+      }
+    }
+  },
+  actions: {
+    resetKeyValue({ commit }, { module, key }) {
+      let value = JSON.parse(sessionStorage.getItem("initialize"))[module][key];
+      console.log(value);
+      commit("setKeyValue", {
+        module,
+        key,
+        value,
+        merge: false
+      });
+    }
+  },
   plugins: [vuexLocal.plugin],
-  modules: modules
+  modules
 });
+
+export const setStoreValue = (module, key, value, merge = true) => {
+  store.commit("setKeyValue", {
+    module,
+    key,
+    value,
+    merge
+  });
+};
+
+export const resetStoreValue = (module, key) => {
+  if (!module && !key) {
+    let defaultState = JSON.parse(sessionStorage.getItem("vuex"));
+    Object.keys(store.state).forEach(m => {
+      store.state[m] = defaultState[m];
+    });
+    return;
+  }
+  if (!!module && !key) {
+    let defaultState = JSON.parse(sessionStorage.getItem("defaultState"));
+    store.state[module] = defaultState[module];
+    return;
+  }
+  store.dispatch("resetKeyValue", {
+    module,
+    key
+  });
+};
+
+Vue.prototype.$setStoreValue = setStoreValue;
+Vue.prototype.$resetStoreValue = resetStoreValue;
+if (!sessionStorage.getItem("initialize")) {
+  sessionStorage.setItem("initialize", JSON.stringify(store.state));
+}
+export default store;
