bc62463506a229917b50015815c5d58279e149d2
chenc <chenc@fengyuntec.com>
2020-10-12 13:00:42 +0800

    refactor: demo.service 修改

--- src/services/demo.service.js
+++ src/services/demo.service.js
@@ -1,9 +1,5 @@
 import http from "../plugins/http";
 
-const demo = {
-  GET_DEMO() {
-    return http.get("/v1/bpi/currentprice.json");
-  }
-};
-
-export default demo;
+export function GET_DEMO() {
+  return http.get("/v1/bpi/currentprice.json");
+}
